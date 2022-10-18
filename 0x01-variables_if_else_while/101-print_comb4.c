56265d847eaacd1651b1f2db4ba09e762c57e438
Olaoye Ayomide <olaoyeayomide061@gmail.com>
2022-10-17 18:47:45 -0700

    Task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,67 +1,38 @@
-int main(void)                                                               
+#include<stdio.h>
+
 /**
- *
- * main - Entry point
- * Return: Always 0 (success)
+ *main - prints all possible different combinations of three digits.
+ *Return: Always 0 (success)
  */
 
 int main(void)
-{                                                                                          
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
 
-	        int m,i,j;                                                                         
-
-		                                                                                           
-
-		        for(i = '0'; i <= '9'; i++)                                                        
-
-				        {                                                                                  
-
-						        for(j ='0'; j <= '9'; j++)                                                         
-
-								        {                                                                                  
-
-										        for(m = '0'; m <= '9'; m++)                                                        
-
-												        {                                                                                  
-
-														                putchar(i);                                                                
-
-																                putchar(j);                                                                
-
-																		                putchar(m);                                                                
-
-																				                                                                                           
-
-																				                if(i == '7' && j == '8' && m == '9')                                       
-
-																							        {                                                                                  
-
-																									                break;                                                                     
-
-																											        }                                                                                  
-
-																						                else;                                                                      
-
-																								        {                                                                                  
-
-																										                putchar(');                                                                
-
-																												                putchar(');                                                                
-
-																														        }                                                                                  
-
-																									        }                                                                                  
-
-											      }                                                                                    
-
-							    }                                                                                      
-
-}                                                                                          
-
-                                                                                           
-
-         putchar('\n');                                                                    
-
-	                                                                                            
-
-	          return (0);
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundreds > tens)))/*eliminatess repitition*/
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && 
+								tens == '8'))/*adds commas and spaces*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
