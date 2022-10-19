b05438fc26e93a9857d39bc1a689b87c454fdc69
Olaoye Ayomide <olaoyeayomide061@gmail.com>
2022-10-19 09:35:34 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - Entry point
+ * function putchar puts the character
+ * Return: 0 if no error, non zero if error
+ */
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+	return (0);
+}
