828286e44f07e96a677c6fd254b8bdd5b941f13d
Olaoye Ayomide <olaoyeayomide061@gmail.com>
2022-10-19 20:44:53 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * main - prints the alphabet in lowerase
+ *
+ * Return : Always 0
+ */
+
+void print_alphabet(void);
+{
+	int j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+	{
+		_putchar(j);
+}
+       _putchar('\n');
+    
+	return (0);
+}
